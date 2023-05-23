98c0374face832583b5fed44419331c95d213ef4
pack <btjdghks2@gmail.com>
2023-05-23 22:38:14 +0900

    junit5 test

--- src/test/java/com/example/startProject/StartProjectApplicationTests.java
+++ src/test/java/com/example/startProject/StartProjectApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.startProject;
 
+import com.example.startProject.controller.adminController;
+import com.example.startProject.service.adminService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(adminController.class)
 class StartProjectApplicationTests {
 
+
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	public void adminhome() throws Exception {
+
+		String hello = "hello";
+
+		mvc.perform(get("/admin/check"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(hello));
+
 	}
+
+
+
 
 }
